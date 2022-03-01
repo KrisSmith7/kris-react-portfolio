898a0096474095cf26aa0e4826b5a9ade7ef3136
Kris Smith <91396141+KrisSmith7@users.noreply.github.com>
2022-02-28 23:55:41 -0500

    rendering nav and about, bio to page

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import bioImage from "../../assets/images/bio.jpg" 
+
+function About() {
+    return (
+        <section>
+            <img className="App-logo" src={bioImage} />
+            <h1>Full-Stack Web Dev</h1>
+            <p>Howdy. New to web development and design, but always eager to jump in. I love the logic behind it and have always been so fascinated by the amazing things we can create. When I'm not working or practicing web dev, you can usually find me with my family, watching a game, or hunting down a good trivia spot.</p>
+        </section>
+    );
+};
+
+export default About;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav () {
+    return (
+        <nav>
+            <ul>
+                <li><a>About</a></li>
+                <li><a>Portfolio</a></li>
+                <li><a>Contact</a></li>
+                <li><a>Resume</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
