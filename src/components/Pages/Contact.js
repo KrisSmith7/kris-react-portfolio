b3de78e38ab9bbf9f0e58a8f510aa31b8290b52a
Kris Smith <91396141+KrisSmith7@users.noreply.github.com>
2022-03-01 16:43:19 -0500

    captures user info onChange

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Contact () {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const {name, email, message} = formState;
+
+    function inputChange(event) {
+        setFormState({...formState, [event.target.name]: event.target.value})
+    }
+console.log (formState)
     return (
         <section>
         <h1>Contact me</h1>
         <form id="contact-form">
           <div>
             <label htmlFor="name">Name:</label>
-            <input type="text" name="name" />
+            <input type="text" name="name" defaultValue={name} onChange={inputChange} />
           </div>
           <div>
             <label htmlFor="email">Email address:</label>
-            <input type="email" name="email" />
+            <input type="email" name="email" defaultValue={email} onChange={inputChange} />
           </div>
           <div>
             <label htmlFor="message">Message:</label>
-            <textarea name="message" rows="5" />
+            <textarea name="message" rows="5" defaultValue={message} onChange={inputChange} />
           </div>
           <button type="submit">Submit</button>
         </form>
