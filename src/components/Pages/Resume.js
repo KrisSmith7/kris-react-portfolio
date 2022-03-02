bb6e90bdd9b9821af544dfb7f8d15b1c5f021a9d
Kris Smith <91396141+KrisSmith7@users.noreply.github.com>
2022-03-02 00:26:50 -0500

    added resume and download link to Resume

--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -1,8 +1,17 @@
 import React from "react";
+import resPhoto from "../../assets/images/resume/resumephoto.png";
 
 function Resume () {
     return (
-        <p> Here's my resume! </p>
+        <div>
+           <p> Click image to download a copy of my resume.</p> 
+         <a
+        href="src\assets\images\resume\ksmithresumedownload.pdf"
+        download
+      >
+        <img className="resume-pic" src={resPhoto} alt="resume of developer"/>
+      </a>
+        </div>
     );
 };
 
