01e05f71b9022368c4fe24ed78fca0950a76b7fe
Kris Smith <91396141+KrisSmith7@users.noreply.github.com>
2022-03-05 10:24:38 -0500

    styled footer icons to purple

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,24 +2,26 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer className='py-4 flex justify-center text-center'>
+        <footer className='py-4 bg-gray w-full h-full'>
+         <div className="w-full h-full flex justify-center">
           <div className='px-4'>
           <a href='https://github.com/KrisSmith7' target="_blank">
-            <i className="bi-github"></i>
+            <i className="bi-github icon-purple scale-125"></i>
           </a>
           </div>
           <div className='px-4'>
           <a href='https://www.linkedin.com/in/kristiansmith220/' target="_blank">
-          <i className="bi bi-linkedin"></i>
+          <i className="bi bi-linkedin icon-purple"></i>
           </a>
           </div>
       
           <div className='px-4'>
           <a href='https://stackoverflow.com/users/17004629/krissmith7' target="_blank">
-          <i className="bi bi-stack-overflow"></i>
+          <i className="bi bi-stack-overflow icon-purple"></i>
           </a>
           </div>
-       
+      </div>
+
       </footer>
     )
 };
