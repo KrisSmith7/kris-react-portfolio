37efe15542102b93f23c1167f233b5ad4c3adb34
Kris Smith <91396141+KrisSmith7@users.noreply.github.com>
2022-03-06 11:01:15 -0500

    removed animation from home page and deployed app

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -6,10 +6,9 @@ function Home () {
         <section className="h-screen">
             <div className="hero-pattern bg-repeat-round h-full w-full flex items-center justify-center">
 
-          <div className="bg-royal-purple/25 w-full h-1/5 flex items-center justify-center">
+          <div className="bg-royal-purple w-full h-1/5 flex items-center justify-center">
 
-            <h1 className="text-gray text-justify normal-case         
-            animate-[bounce_3s_infinite]">
+            <h1 className="text-gray text-justify normal-case">
                 Hey there! I'm Kris and I'm a webDeveloper( )</h1>
                     
                 </div>
